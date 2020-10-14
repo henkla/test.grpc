17a2a2a8c0df3a6f08607f6211976e02b7bb8399
henkla <lthlarsson@gmail.com>
2020-10-14 21:43:11 +0200

    adding solution

---
+++ Grpc.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using Grpc.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            // generatePassword
+            var generateRequest = new GenerateRequest
+            {
+                PasswordLength = 12,
+                Casing = Casing.Mixed,
+                UseSpecials = false,
+                UseNumerics = false
+            };
+            var passwordGeneratorClient = new Generator.GeneratorClient(channel);
+            var generateReply = await passwordGeneratorClient.GenerateAsync(generateRequest);
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                generateReply = await passwordGeneratorClient.GenerateAsync(generateRequest);
+                Console.Clear();
+                Console.WriteLine($"Generated password is:\n{generateReply.GeneratedPassword}");
+            }
+        }
+    }
+}
---
+++ Grpc.Server/Services/GeneratorService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Grpc.Server
+{
+    public class GeneratorService : Generator.GeneratorBase
+    {
+        private readonly ILogger<GeneratorService> _logger;
+        private readonly SimplePasswordGenerator.Generator _generator;
+
+        public GeneratorService(ILogger<GeneratorService> logger)
+        {
+            _logger = logger;
+            _generator = new SimplePasswordGenerator.Generator();
+        }
+
+        public override Task<GenerateReply> Generate(GenerateRequest request, ServerCallContext context)
+        {
+            var password = _generator.Generate(passwordLength: request.PasswordLength,
+                                               casing: (SimplePasswordGenerator.Casing)request.Casing,
+                                               useSpecials: request.UseSpecials,
+                                               useNumerics: request.UseNumerics,
+                                               filter: request.Filter);
+
+            return Task.FromResult(new GenerateReply { GeneratedPassword = password });
+        }
+
+    }
+}
